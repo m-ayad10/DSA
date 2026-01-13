06e6c4a12571f275cd20a926c87d136244555dfe
Mohammed Ayad <144673827+m-ayad10@users.noreply.github.com>
2026-01-13 10:57:38 +0530

    Time: 9 ms (24.79%) | Memory: 55.7 MB (82.73%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    let freq=new Map([['b',0],['a',0],['l',0],['n',0]])
+    let str='balloon'
+    for(let i=0;i<text.length;i++)
+    {
+        if(!str.includes(text[i])) continue
+        let count=(freq.get(text[i])||0)+1
+        freq.set(text[i],count)
+    }
+    let curr=null;
+    for(let [k,v] of freq)
+    {
+        if((k=='l'||k=='o')&&(Math.floor(v/2)<curr||curr==null))
+        {
+            curr=(Math.floor(v/2))
+        }
+        else if(v<curr||curr==null)
+        {
+            curr=v
+        }
+    }
+    return curr
+};

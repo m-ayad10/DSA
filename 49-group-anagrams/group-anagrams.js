8f4e34cfa6cb806ee604bcd99fe5c58e3db2b47a
Mohammed Ayad <144673827+m-ayad10@users.noreply.github.com>
2026-01-20 11:17:02 +0530

    Time: 53 ms (12.14%) | Memory: 68.9 MB (34.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,24 +2,38 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-    let maps=new Map()
-    for(let i=0;i<strs.length;i++)
+
+
+function getFreqStr(strs) {
+    let freq = new Array(26).fill(0)
+    for(let ch of strs )
     {
-        let sorted=strs[i].split('').sort().join('')
-        if(!maps.has(sorted))
+        freq[ch.charCodeAt(0)-97]++
+    }
+    let freqStr = ''
+    for(let i in freq)
+    {
+        if(freq[i]>0)
         {
-            maps.set(sorted,[strs[i]])
-        }
-        else
-        {
-            let arr=maps.get(sorted)
-            maps.set(sorted,[...arr,strs[i]])
+            freqStr+= (String.fromCharCode(i+97)+freq[i])
         }
     }
-    let result=[]
-    for(let [k,v] of maps)
-    {
+    return freqStr
+}
+var groupAnagrams = function (strs) {
+    let maps = new Map()
+    for (let i = 0; i < strs.length; i++) {
+        let freqStr =  getFreqStr(strs[i])
+        if (!maps.has(freqStr)) {
+            maps.set(freqStr, [strs[i]])
+        }
+        else {
+            let arr = maps.get(freqStr)
+            maps.set(freqStr, [...arr, strs[i]])
+        }
+    }
+    let result = []
+    for (let [k, v] of maps) {
         result.push(v)
     }
     return result

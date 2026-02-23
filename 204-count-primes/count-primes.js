5717f47dbff2098e135a00535f62bde2cb0c8163
Mohammed Ayad <144673827+m-ayad10@users.noreply.github.com>
2026-02-23 11:12:47 +0530

    Time: 209 ms (46.96%) | Memory: 95.8 MB (56.90%) - LeetSync

---
+++ 204-count-primes/count-primes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+    if (n < 2) return 0
+    let arr = new Array(n).fill(1)
+    for(let i=2;i<arr.length;i++)
+    {
+        if(arr[i]==1)
+        {
+            for(let j=i*i;j<arr.length;j+=i)
+            {
+                arr[j]=0
+            }
+        }
+    }
+    let count=0
+    for(let i=2;i<arr.length;i++)
+    {
+        if(arr[i]==1) count++
+    }
+    return count
+};

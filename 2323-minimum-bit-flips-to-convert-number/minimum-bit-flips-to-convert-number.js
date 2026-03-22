1365c447c8561a4857e78d72d39da987e1905f40
Mohammed Ayad <144673827+m-ayad10@users.noreply.github.com>
2026-03-22 17:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (88.99%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.js
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.js
@@ -5,43 +5,13 @@
  */
 
 var minBitFlips = function (start, goal) {
-    let first = ''
-    let second = ''
-    while (start > 0) {
-        if ((start & 1) == 1) {
-            first = 1 + first
-        }
-        else {
-            first = 0 + first
-        }
-        start = start >> 1
-    }
-    while (goal > 0) {
-        if ((goal & 1) == 1) {
-            second = 1 + second
-        }
-        else {
-            second = 0 + second
-        }
-        goal = goal >> 1
-    }
-    let i = first.length - 1
-    let j = second.length - 1
-    let count = 0
-    while (i >= 0 && j >= 0) {
-        if (first[i--] != second[j--]) {
-            count++
-        }
-    }
-    if (i != -1) {
-        while (i >= 0) {
-            if (first[i--] == 1) count++
-        }
-    }
-    else {
-        while (j >= 0) {
-            if (second[j--] == 1) count++
-        }
+    let count=0;
+    start=start ^ goal
+    while(start>0)
+    {
+        if((start&1)==1) count++
+        start=start>>1
     }
     return count
 };
+
